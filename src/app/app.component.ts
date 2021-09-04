b8b115d1d7194dec49fa60e3fa59218637fe112a
Alessandro Tulioli <alessandro.smarapd@hotmail.com>
2021-09-03 21:33:17 -0300

    Cria funcionalidades e formulário

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ListaTarefas } from 'src/models/listaTarefas.models';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +9,57 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  public listaTarefas: any[] = [];
+  public listaTarefas: ListaTarefas[] = [];
+  public title: String = 'Tarefas';
+  public form: FormGroup;
 
-  constructor() {
-    this.listaTarefas.push('Comprar carvão');
-    this.listaTarefas.push('Abastecer o carro');
-    this.listaTarefas.push('Pagar o condomínio');
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    });
+    this.load();
+  }
+
+  adicionar(){
+    const titulo = this.form.controls['title'].value;
+    const index = this.listaTarefas.length + 1;
+    this.listaTarefas.push(new ListaTarefas(index, titulo, false));
+    this.salvar();
+    this.limpar();
+  }
+
+  remover(tarefa: ListaTarefas){
+    const index = this.listaTarefas.indexOf(tarefa);
+    if(index !== -1){
+      this.listaTarefas.splice(index, 1);
+    }
+    this.salvar();
+  }
+
+  marcarConcluido(tarefa: ListaTarefas){
+    tarefa.done = true;
+    this.salvar();
+  }
+
+  marcarNaoConcluido(tarefa: ListaTarefas){
+    tarefa.done = false;
+    this.salvar();
+  }
+
+  limpar(){
+    this.form.reset();
+  }
+
+  salvar(){
+    const dados = JSON.stringify(this.listaTarefas);
+    localStorage.setItem('listaTarefas', dados);
+  }
+
+  load(){
+    this.listaTarefas = JSON.parse(localStorage.getItem('listaTarefas') || '{}');
   }
 }
---
+++ src/models/listaTarefas.models.ts
@@ -0,0 +1,8 @@
+export class ListaTarefas {
+
+  constructor(
+    public id: Number,
+    public title: String,
+    public done: Boolean
+  ) { }
+}
